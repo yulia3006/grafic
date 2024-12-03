95f21b4d91c89329cf6eba0af55bf27e087838ac
yulia3006 <bochkaryova.julia@gmail.com>
2024-12-03 18:52:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+﻿#include <SFML/Graphics.hpp>
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Massive");
+	const int n = 10;  
+	const int cellSize = 50;
+	sf::RectangleShape cells[n][n];
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			cells[i][j].setSize(sf::Vector2f(cellSize - 2, cellSize - 2));
+			cells[i][j].setPosition(i * cellSize, j * cellSize);
+			cells[i][j].setFillColor(sf::Color::White);
+			cells[i][j].setOutlineColor(sf::Color::Black);
+			cells[i][j].setOutlineThickness(1);
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i + j == n - 1) {
+				cells[i][j].setFillColor(sf::Color::Green);
+			}
+		}
+	}
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear(sf::Color::White);
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				window.draw(cells[i][j]);
+
+			}
+		}
+		window.display();
+	}
+	return 0;
+}
